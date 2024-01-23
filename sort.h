6ce05dc1932537e46aae279a68d7064f45c5f8da
foubadev <foubabertrand@gmail.com>
2024-01-23 07:58:25 +0100

    third commit

--- sort.h
+++ sort.h
@@ -23,5 +23,5 @@ listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+int lomuto_partition_scheme(int *array, size_t size, int left, int right);
 #endif
